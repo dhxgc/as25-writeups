46249aefdaca292dd0bb579746893fa86602ae89
dhxgc <132298839+dhxgc@users.noreply.github.com>
2025-03-23 01:36:56 +0500

    Delete scipts/other_docker/pgsql directory

--- scipts/other_docker/pgsql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER superadmin WITH SUPERUSER CREATEDB LOGIN PASSWORD 'P@ssw0rdSkills';
-
-CREATE USER zabbix_user WITH LOGIN PASSWORD 'P@ssw0rdSkills';
-CREATE USER gitflic_user WITH LOGIN PASSWORD 'P@ssw0rdSkills';
-CREATE USER kc_user WITH LOGIN PASSWORD 'P@ssw0rdSkills';
-
-CREATE DATABASE zabbix_db OWNER zabbix_user;
-CREATE DATABASE gitflic_db OWNER gitflic_user;
-CREATE DATABASE kc_db OWNER kc_user;
